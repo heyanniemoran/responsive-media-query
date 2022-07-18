abbcefc7a852bb0ec2e50d6e51b6ce2c2f138196
heyanniemoran <heyanniemoran@gmail.com>
2022-07-18 18:02:54 +0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,16 @@
-/// <reference types="react" />
-interface Props {
-    text: string;
+import React from 'react';
+export default function useMediaQuery(obj: {
+    query: string;
+}): boolean;
+interface MediaProps {
+    orientation?: string;
+    minWidth?: number;
+    maxWidth?: number;
+    minHeight?: number;
+    maxHeight?: number;
+    minResolution?: string | number;
+    maxResolution?: string | number;
+    children: React.ReactNode | ((matches: boolean) => React.ReactNode);
 }
-export declare const ExampleComponent: ({ text }: Props) => JSX.Element;
+export declare function MediaQuery(props: MediaProps): JSX.Element | null;
 export {};
--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,13 +1,45 @@
-import { createElement } from 'react';
+import React, { useState, useEffect } from 'react';
 
-var styles = {"test":"_3ybTi"};
+function useMediaQuery(obj) {
+  var match_media = window.matchMedia(obj.query).matches;
 
-var ExampleComponent = function ExampleComponent(_ref) {
-  var text = _ref.text;
-  return createElement("div", {
-    className: styles.test
-  }, "Example Component: ", text);
-};
+  var _useState = useState(match_media),
+      state = _useState[0],
+      setState = _useState[1];
 
-export { ExampleComponent };
+  useEffect(function () {
+    window.addEventListener('resize', function () {
+      var match_media_effect = window.matchMedia(obj.query).matches;
+      setState(match_media_effect);
+    });
+  });
+  return state;
+}
+function MediaQuery(props) {
+  var query_array = [];
+  var orientation = props.orientation;
+  if (orientation) query_array.push('(orientation: ' + orientation + ')');
+  var minWidth = props.minWidth;
+  if (minWidth) query_array.push('(min-width: ' + minWidth + 'px)');
+  var maxWidth = props.maxWidth;
+  if (maxWidth) query_array.push('(max-width: ' + maxWidth + 'px)');
+  var minHeight = props.minHeight;
+  if (minHeight) query_array.push('(min-height: ' + minHeight + 'px)');
+  var maxHeight = props.maxHeight;
+  if (maxHeight) query_array.push('(max-height: ' + maxHeight + 'px)');
+  var minResolution = props.minResolution;
+  if (minResolution) query_array.push('(' + (typeof minResolution === 'string' ? 'min-resolution' : '-webkit-min-device-pixel-ratio') + ': ' + minResolution + ')');
+  var maxResolution = props.maxResolution;
+  if (maxResolution) query_array.push('(' + (typeof maxResolution === 'string' ? 'max-resolution' : '-webkit-max-device-pixel-ratio') + ': ' + maxResolution + ')');
+  var query = query_array.join(' and ');
+  var matches = useMediaQuery({
+    query: query
+  });
+  if (typeof props.children == 'function') return React.createElement("div", null, props.children(matches));
+  if (!matches) return null;
+  return React.createElement("div", null, props.children);
+}
+
+export default useMediaQuery;
+export { MediaQuery };
 //# sourceMappingURL=index.modern.js.map
